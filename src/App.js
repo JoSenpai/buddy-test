876badb539056bb9801d1a37e4054426ff99395a
Jonathan Yong <jonathan.yong@papercut.com>
2020-09-17 10:36:47 +1000

    forced push;

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          FORCE PUSH Learn React
         </a>
       </header>
     </div>
